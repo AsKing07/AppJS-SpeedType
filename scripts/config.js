1e5f07a059a3a7e4b642466b92d956d9982107a1
AsKing07 <charbelsnn@gmail.com>
2023-07-19 19:23:15 +0100

    Ajout d'autres mots et phrases

--- scripts/config.js
+++ scripts/config.js
@@ -4,29 +4,175 @@
  * En particulier les listes de mots et de phrases proposés à l'utilisateur
  * 
  *********************************************************************************/
+
  // Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
 const listeMots = genererMotsAleatoires(10);
 const listePhrases = genererPhrasesAleatoires(10);
+
  // Fonction pour générer une liste aléatoire de mots
-function genererMotsAleatoires(maximum) {
+function genererMotsAleatoires(maximum) 
+{
   const motsAleatoires = [];
-  const motsProposes = ["Voiture", "Maison", "Chien", "Chat", "Ordinateur", "Livre", "Jardin", "Musique", "Voyage", "Cuisine", "Montagne", "Plage", "Film", "Sport", "Amour", "Famille", "Travail", "Etude", "Nature", "Art","Portail","Limite", "Football"]; // Liste des mots proposés
+
+
+  const motsProposes = [
+    "Voiture",
+    "Maison",
+    "Chien",
+    "Chat",
+    "Ordinateur",
+    "Livre",
+    "Jardin",
+    "Musique",
+    "Voyage",
+    "Cuisine",
+    "Montagne",
+    "Plage",
+    "Film",
+    "Sport",
+    "Amour",
+    "Famille",
+    "Travail",
+    "Etude",
+    "Nature",
+    "Art",
+    "Portail",
+    "Limite",
+    "Football",
+    "Bonheur",
+    "Rêve",
+    "Aventure",
+    "Liberté",
+    "Sourire",
+    "Amitié",
+    "Nature",
+    "Sagesse",
+    "Patience",
+    "Créativité",
+    "Connaissance",
+    "Joie",
+    "Bienveillance",
+    "Harmonie",
+    "Optimisme",
+    "Tolérance",
+    "Temps",
+    "Empathie",
+    "Curiosité",
+    "Partage",
+    "Générosité",
+    "Gratitude",
+    "Confiance",
+    "Respect",
+    "Silence",
+    "Méditation",
+    "Sommeil",
+    "Équipe",
+    "Rêve",
+    "Gourmandise",
+    "Tranquillité",
+    "Réussite",
+    "Défi",
+    "Succès",
+    "Entraide",
+    "Bien-être",
+    "Équilibre"
+  ];//Liste de mots possibles
+  
+  
   const motsCopie = [...motsProposes]; // Créer une copie de la liste originale
+
    // Sélectionner de manière aléatoire les mots et les ajouter à la liste aléatoire
-  while (motsAleatoires.length < maximum && motsCopie.length > 0) {
+  while (motsAleatoires.length < maximum && motsCopie.length > 0) 
+  {
     const indexAleatoire = Math.floor(Math.random() * motsCopie.length);
     const motAleatoire = motsCopie.splice(indexAleatoire, 1)[0];
     motsAleatoires.push(motAleatoire);
   }
    return motsAleatoires;
 }
+
+
+
+
+
+
  // Fonction pour générer une liste aléatoire de phrases
-function genererPhrasesAleatoires(maximum) {
+function genererPhrasesAleatoires(maximum) 
+{
   const phrasesAleatoires = [];
-  const phrasesProposees = ["C'est une belle journée.", "Je suis heureux.", "Vive la vie.", "Quel temps magnifique !", "J'adore voyager.", "La musique me fait du bien.", "J'aime cuisiner.", "Le jardin est un endroit paisible.", "Les animaux sont adorables.", "La lecture est enrichissante.", "Je suis passionné par l'art.", "Le sport me motive.", "La famille est importante.", "Je travaille dur pour réussir.", "Je suis en train d'étudier pour mon examen.", "La nature est magnifique.", "J'aime regarder des films.", "L'amour est essentiel.", "La montagne est impressionnante.", "La plage est relaxante."]; // Liste des phrases proposées
+
+  const phrasesProposees = [
+    "C'est une belle journée.",
+    "Je suis heureux.",
+    "Vive la vie.",
+    "Quel temps magnifique !",
+    "J'adore voyager.",
+    "La musique me fait du bien.",
+    "J'aime cuisiner.",
+    "Le jardin est un endroit paisible.",
+    "Les animaux sont adorables.",
+    "La lecture est enrichissante.",
+    "Je suis passionné par l'art.",
+    "Le sport me motive.",
+    "La famille est importante.",
+    "Je travaille dur pour réussir.",
+    "Je suis en train d'étudier pour mon examen.",
+    "La nature est magnifique.",
+    "J'aime regarder des films.",
+    "L'amour est essentiel.",
+    "La montagne est impressionnante.",
+    "La plage est relaxante.",
+    "La vie est pleine de surprises.",
+    "La patience est une vertu.",
+    "Le sourire est contagieux.",
+    "Le rire est la meilleure thérapie.",
+    "La simplicité rend heureux.",
+    "La gentillesse est une force.",
+    "Le respect est fondamental.",
+    "La créativité nourrit l'âme.",
+    "La persévérance mène au succès.",
+    "La sagesse s'acquiert avec l'expérience.",
+    "L'espoir donne des ailes.",
+    "La liberté est précieuse.",
+    "Le courage permet de surmonter les obstacles.",
+    "L'amitié est un trésor.",
+    "La curiosité ouvre de nouvelles portes.",
+    "Le silence est parfois réparateur.",
+    "L'effort est récompensé.",
+    "Le partage crée des liens.",
+    "La générosité illumine le monde.",
+    "La gratitude transforme notre regard.",
+    "Le respect de la nature est essentiel.",
+    "L'amour est le moteur de la vie.",
+    "Le calme apaise l'esprit.",
+    "La confiance en soi est libératrice.",
+    "Le savoir est une arme puissante.",
+    "La joie est contagieuse.",
+    "La bienveillance embellit les relations.",
+    "Le voyage élargit les horizons.",
+    "La découverte est une aventure.",
+    "La gourmandise est un plaisir.",
+    "Le sommeil est réparateur.",
+    "La méditation apaise l'âme.",
+    "L'optimisme ouvre des portes.",
+    "La persévérance conduit au succès.",
+    "Le respect des autres est primordial.",
+    "L'empathie renforce les liens.",
+    "La tolérance favorise l'harmonie.",
+    "Le temps guérit les blessures.",
+    "La patience porte ses fruits.",
+    "Le travail en équipe est enrichissant.",
+    "La réussite est le fruit de l'effort.",
+    "Le rêve donne des ailes.",
+    "La créativité est une source d'épanouissement."
+  ]; // Liste des phrases proposées
+ 
+ 
   const phrasesCopie = [...phrasesProposees]; // Créer une copie de la liste originale
+
    // Sélectionner de manière aléatoire les phrases et les ajouter à la liste aléatoire
-  while (phrasesAleatoires.length < maximum && phrasesCopie.length > 0) {
+  while (phrasesAleatoires.length < maximum && phrasesCopie.length > 0) 
+  {
     const indexAleatoire = Math.floor(Math.random() * phrasesCopie.length);
     const phraseAleatoire = phrasesCopie.splice(indexAleatoire, 1)[0];
     phrasesAleatoires.push(phraseAleatoire);
--- scripts/script.js
+++ scripts/script.js
@@ -1,148 +1,134 @@
-
-
 /**
- * Cette fonction construit et affiche l'email. 
- * @param {string} nom : le nom du joueur
- * @param {string} email : l'email de la personne avec qui il veut partager son score
- * @param {string} score : le score. 
+ * Cette fonction construit et affiche l'email de partage du score.
+ * @param {string} nom - Le nom du joueur.
+ * @param {string} email - L'email de la personne avec qui le score est partagé.
+ * @param {string} score - Le score réalisé.
  */
-function afficherEmail(nom, email, score) 
-{
-    let mailto = `mailto:${email}?subject=Partage du score SpeedType&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site de SpeedType ! Viens essayer de battre mon score sur https://asking07.github.io/AppJS-SpeedType/`
-    location.href = mailto
-}
-
-
-/**
- * Cette fonction prend un nom en paramètre et valide qu'il est au bon format
- * ici : deux caractères au minimum
- * @param {string} nom 
- * @return {boolean}
- */
-function validerNom(nom) {
-    if (nom.length < 2) {
-        throw new Error("Le nom est trop court. ")
+ function afficherEmail(nom, email, score) 
+  {
+        let mailto = `mailto:${email}?subject=Partage du score SpeedType&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site de SpeedType ! Viens essayer de battre mon score sur https://asking07.github.io/AppJS-SpeedType/`;
+        location.href = mailto;
+  }
+  
+  /**
+   * Valide si le nom est au bon format (au moins deux caractères).
+   * @param {string} nom - Le nom à valider.
+   * @throws {Error} Le nom est trop court.
+   */
+  function validerNom(nom) 
+  {
+    if (nom.length < 2) 
+    {
+      throw new Error("Le nom est trop court.");
     }
-}
-
-/**
- * Cette fonction prend un email en paramètre et valide qu'il est au bon format. 
- * @param {string} email 
- * @return {boolean}
- */
-function validerEmail(email) {
-    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
-    if (!emailRegExp.test(email)) {
-        throw new Error("L'email n'est pas valide.")
+  }
+  
+  /**
+   * Valide si l'email est au bon format.
+   * @param {string} email - L'email à valider.
+   * @throws {Error} L'email n'est pas valide.
+   */
+  function validerEmail(email) 
+  {
+    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
+    if (!emailRegExp.test(email)) 
+    {
+      throw new Error("L'email n'est pas valide.");
     }
-}
-
-/**
- * Cette fonction affiche le message d'erreur passé en paramètre. 
- * Si le span existe déjà, alors il est réutilisé pour ne pas multiplier
- * les messages d'erreurs. 
- * @param {string} message 
- */
-function afficherMessageErreur(message) {
-    
-    let spanErreurMessage = document.getElementById("erreurMessage")
-
-    if (!spanErreurMessage) {
-        let popup = document.querySelector(".popup")
-        spanErreurMessage = document.createElement("span")
-        spanErreurMessage.id = "erreurMessage"
-        
-        popup.append(spanErreurMessage)
+  }
+  
+  /**
+   * Affiche le message d'erreur passé en paramètre.
+   * Si le span existe déjà, il est réutilisé pour éviter les doublons.
+   * @param {string} message - Le message d'erreur à afficher.
+   */
+  function afficherMessageErreur(message) 
+  {
+    let spanErreurMessage = document.getElementById("erreurMessage");
+  
+    if (!spanErreurMessage) 
+    {
+      let popup = document.querySelector(".popup");
+      spanErreurMessage = document.createElement("span");
+      spanErreurMessage.id = "erreurMessage";
+      popup.append(spanErreurMessage);
     }
-    
-    spanErreurMessage.innerText = message
-}
-
-
-function gererFormulaire(scoreEmail)
-{
+  
+    spanErreurMessage.innerText = message;
+  }
+  
+  /**
+   * Gère la soumission du formulaire de partage de score.
+   * @param {string} scoreEmail - Le score à partager.
+   */
+  function gererFormulaire(scoreEmail) 
+  {
     try 
     {
-        let baliseNom = document.getElementById("nom")
-        let nom = baliseNom.value
-        validerNom(nom)
-
-        let baliseEmail = document.getElementById("email")
-        let email = baliseEmail.value
-        validerEmail(email)
-        afficherMessageErreur("")
-        afficherEmail(nom, email, scoreEmail)
-
+      let baliseNom = document.getElementById("nom");
+      let nom = baliseNom.value;
+      validerNom(nom);
+  
+      let baliseEmail = document.getElementById("email");
+      let email = baliseEmail.value;
+      validerEmail(email);
+      afficherMessageErreur("");
+      afficherEmail(nom, email, scoreEmail);
     } 
-    catch(erreur) 
+    catch (erreur) 
     {
-        afficherMessageErreur(erreur.message)
+      afficherMessageErreur(erreur.message);
     }
-}
-
-
-
-function verifierMot(motApp,motUser,scores) 
-{
-
-    switch (motUser) {
-        case motApp:
-            alert("Bravo!Continuez comme ça")
-            console.log("Bravo !")
-            scores[0]+=1
-            break
-        case "Gredin":
-            console.log("Restez correct !")
-            break
-        case "Mécréant":
-            console.log("Restez correct !")
-            break
-        case "Vilain":
-            console.log("Soyez gentil !")
-            break
-        default:
-            console.log("Vous avez fait une erreur de frappe.")
-            alert("Vous avez fait une erreur de frappe")
-    }
-    
-}
-
-function afficherResultats(score, nombreQuestions) 
-{
-      // Récupération de la zone dans laquelle on va écrire le score
-      let spanScore = document.querySelector(".zoneScore span")
-      // Ecriture du texte
-      let affichageScore = `${score} / 80` 
-      // On place le texte à l'intérieur du span. 
-      spanScore.innerText = affichageScore
-    let message = 'Votre score est de ' + score + ' sur 80' 
-    console.log(message)
-}
-
-
-
-function afficherProposition(mot)
-{
-    let affichageMot = document.querySelector(".zoneProposition")
-    affichageMot.innerText = mot
-}
-
-
-
-function lancerJeu() {
+  }
+  
+  
+  /**
+   * Affiche les résultats du jeu (score obtenu sur le nombre total de questions).
+   * @param {number} score - Le score réalisé.
+   * @param {number} nombreQuestions - Le nombre total de questions.
+   */
+  function afficherResultats(score, nombreQuestions) {
+    let spanScore = document.querySelector(".zoneScore span");
+    let affichageScore = `${score} / 80`;
+    spanScore.innerText = affichageScore;
+  
+    let message = `Votre score est de ${score} sur 80`;
+    console.log(message);
+  }
+  
+  /**
+   * Affiche la proposition de mot.
+   * @param {string} mot - Le mot à afficher.
+   */
+  function afficherProposition(mot) 
+  {
+    let affichageMot = document.querySelector(".zoneProposition");
+    affichageMot.innerText = mot;
+  }
+  
+  /**
+   * Lance le jeu SpeedType.
+   */
+  function lancerJeu() 
+  {
     // Initialisations
     let score = 0;
-    let nbMotsProposes = 0;
+    let nbMotsProposes = 0; 
     let listeProposition = listeMots; // Nous utilisons toujours la liste de mots par défaut
-    let total=0;
+    let total = 0; //Le nombre total d'entrées exactes de l'utilisateur
   
     let i = 0;
     let startTime = 0; // Variable pour stocker le temps de début de chaque mot
     let gameIsOver = false; // Variable pour vérifier si le jeu est terminé
 
+    // Initialisation pour le premier mot
+    afficherProposition(listeProposition[i]);
+    startTime = new Date().getTime(); // Début du compteur de temps pour le premier mot
+    afficherScore(score);
   
     // Fonction pour mettre à jour le score et l'afficher à l'écran
-    function afficherScore(score) {
+    function afficherScore(score) 
+    {
       const spanScore = document.querySelector(".zoneScore span");
       spanScore.innerText = score;
     }
@@ -170,10 +156,25 @@ function lancerJeu() {
       const zoneTemps = document.querySelector(".zoneTemps");
       zoneTemps.innerText = `Temps écoulé : ${temps / 1000} secondes`;
     }
+
+    // Affichage du compteur de temps en continu
+    const timerInterval = setInterval(() => 
+    {
+        if (!gameIsOver) 
+        {
+          // Arrêter le compteur lorsque le jeu est terminé
+          const currentTime = new Date().getTime();
+          const tempsEcoule = currentTime - startTime;
+          afficherTempsEcoule(tempsEcoule);
+        }
+      }, 100); // Rafraîchissement toutes les 100 millisecondes
+
+
   
-    // Gestion de l'événement réaliser lorsque l'utilisateur clique sur le bouton valider
+    // Gestion de l'événement réalisé lorsque l'utilisateur clique sur le bouton "Valider"
     const Valider = document.getElementById("btnValiderMot");
-    Valider.addEventListener("click", () => {
+    Valider.addEventListener("click", () => 
+    {
       const motEntre = document.getElementById("inputEcriture");
       const motCourant = listeProposition[i];
   
@@ -184,34 +185,83 @@ function lancerJeu() {
         const scoreMot = calculerScore(tempsEcoule);
   
         score += scoreMot;
-        total+=1;
+        total += 1;
         afficherScore(score);
       }
-     
-       
+  
+      i++;
+      if (listeProposition[i] === undefined) 
+      {
+        let fin = `Le jeu est fini ! ${total} entrées exactes sur 10 propositions totales`;
+        afficherProposition(fin);
+        Valider.disabled = true;
+        nbMotsProposes = listeProposition.length;
+        afficherResultats(score, nbMotsProposes);
+        gameIsOver = true; // Le jeu est terminé, on arrête le compteur de temps
+        clearInterval(timerInterval); // Arrêter le rafraîchissement du compteur
+      } 
+      else 
+      {
+        afficherProposition(listeProposition[i]);
+        motEntre.value = "";
+        startTime = new Date().getTime(); // Mettre à jour le temps de début pour le prochain mot
+      }
+    });
+  
+    
+   
+  
+    // Récupération de l'élément input pour la zone de saisie
+    const inputEcriture = document.getElementById("inputEcriture");
+  
+    // Placer le curseur dans la zone de saisie dès que la page est chargée
+    inputEcriture.focus();
+
+    // Ajouter l'écouteur d'événement "keypress" sur la zone de saisie
+    inputEcriture.addEventListener("keypress", onKeyPress);
+  
+    // Fonction pour gérer l'événement "keypress" sur la zone de saisie
+    function onKeyPress(event) {
+      // Vérifier si la touche pressée est la touche "Entrée" (code 13)
+      if (event.keyCode === 13) 
+      {
+        const motEntre = document.getElementById("inputEcriture");
+        const motCourant = listeProposition[i];
+  
+        if (motEntre.value === motCourant) 
+        {
+          const endTime = new Date().getTime();
+          const tempsEcoule = endTime - startTime;
+          const scoreMot = calculerScore(tempsEcoule);
+  
+          score += scoreMot;
+          total += 1;
+          afficherScore(score);
+        }
   
         i++;
         if (listeProposition[i] === undefined) 
         {
-            let fin = `Le jeu est fini!!!! ${total} entrées exactes sur 10 propositions totales`
-            afficherProposition(fin);
-            Valider.disabled = true;
-            nbMotsProposes = listeProposition.length;
-            afficherResultats(score, nbMotsProposes);
-            gameIsOver = true; // Le jeu est terminé, on arrête le compteur de temps
-            clearInterval(timerInterval); // Arrêter le rafraîchissement du compteur
-        }
+          let fin = `Le jeu est fini ! ${total} entrées exactes sur 10 propositions totales`;
+          afficherProposition(fin);
+          Valider.disabled = true;
+          nbMotsProposes = listeProposition.length;
+          afficherResultats(score, nbMotsProposes);
+          gameIsOver = true; // Le jeu est terminé, on arrête le compteur de temps
+          clearInterval(timerInterval); // Arrêter le rafraîchissement du compteur
+        } 
         else 
         {
           afficherProposition(listeProposition[i]);
           motEntre.value = "";
           startTime = new Date().getTime(); // Mettre à jour le temps de début pour le prochain mot
         }
-      
-      
-    });
+      }
+    }
   
-    // Gestion de l'événement change sur les boutons radios.
+
+
+    // Gestion de l'événement change sur les boutons radios de choix de mot ou phrase.
     const listeBtnRadio = document.querySelectorAll(".optionSource input");
     for (let index = 0; index < listeBtnRadio.length; index++) {
       listeBtnRadio[index].addEventListener("change", (event) => {
@@ -221,89 +271,18 @@ function lancerJeu() {
           listeProposition = listePhrases;
         }
         afficherProposition(listeProposition[i]);
-        startTime = new Date().getTime(); // Début du compteur de temps pour le premier mot ou phrase 
+        startTime = new Date().getTime(); // Début du compteur de temps pour le premier mot ou phrase
         score = 0;
       });
     }
+    
   
-    // Initialisation pour le premier mot
-    afficherProposition(listeProposition[i]);
-    startTime = new Date().getTime(); // Début du compteur de temps pour le premier mot
-    afficherScore(score);
-  
-    // Affichage du compteur de temps en continu
-  const timerInterval = setInterval(() => {
-    if (!gameIsOver) { // Arrêter le compteur lorsque le jeu est terminé
-      const currentTime = new Date().getTime();
-      const tempsEcoule = currentTime - startTime;
-      afficherTempsEcoule(tempsEcoule);
-    }
-  }, 100); // Rafraîchissement toutes les 100 millisecondes
-
-
-
-
-    // Récupération de l'élément input pour la zone de saisie
-  const inputEcriture = document.getElementById("inputEcriture");
-
-  // Placer le curseur dans la zone de saisie dès que la page est chargée
-  inputEcriture.focus();
-
-  // Fonction pour gérer l'événement "keypress" sur la zone de saisie
-  function onKeyPress(event) {
-    // Vérifier si la touche pressée est la touche "Entrée" (code 13)
-    if (event.keyCode === 13) 
-    {
-        const motEntre = document.getElementById("inputEcriture");
-        const motCourant = listeProposition[i];
-    
-        if (motEntre.value === motCourant) 
-        {
-          const endTime = new Date().getTime();
-          const tempsEcoule = endTime - startTime;
-          const scoreMot = calculerScore(tempsEcoule);
-    
-          score += scoreMot;
-          total+=1;
-          afficherScore(score);
-        }
-       
-         
-    
-          i++;
-          if (listeProposition[i] === undefined) 
-          {
-              let fin = `Le jeu est fini!!!! ${total} entrées exactes sur 10 propositions totales`
-              afficherProposition(fin);
-              Valider.disabled = true;
-              nbMotsProposes = listeProposition.length;
-              afficherResultats(score, nbMotsProposes);
-              gameIsOver = true; // Le jeu est terminé, on arrête le compteur de temps
-              clearInterval(timerInterval); // Arrêter le rafraîchissement du compteur
-          }
-          else 
-          {
-            afficherProposition(listeProposition[i]);
-            motEntre.value = "";
-            startTime = new Date().getTime(); // Mettre à jour le temps de début pour le prochain mot
-          }
-    }
+    // Gestion du formulaire de partage de score
+    let form = document.querySelector("form");
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let scoreEmail = `${score} / 80`;
+      gererFormulaire(scoreEmail);
+    });
   }
-
-  // Ajouter l'écouteur d'événement "keypress" sur la zone de saisie
-  inputEcriture.addEventListener("keypress", onKeyPress);
-
-
-  //Gestion du formulaire de partage de score
-  let form = document.querySelector("form")
-  form.addEventListener("submit", (event) => {
-      event.preventDefault()
-      let scoreEmail = `${score} / 80`
-      gererFormulaire(scoreEmail)
-  })
-
-}
   
-
-
-
